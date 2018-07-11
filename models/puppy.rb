4534d0413224f8e6fc622955675dd30c72241645
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-07-11 23:10:41 +0000

    automatically backed up by learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -1,10 +1,10 @@
-class Puppy 
+class Puppy
   attr_accessor :name, :breed, :age
 
   def initialize(name, breed, age)
     @name = name
     @breed = breed
-    @age = age 
+    @age = age
   end
 
 end
